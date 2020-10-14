f1f514319a2125b1e38d05a7689d3a34d0da5fa1
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-14 11:02:28 -0700

    first test

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,10 @@
 const { add } = require('./calculator.js');
 
 // test away!
+
+//should add two numbers 
+
+test("add() should add two number", () =>{
+    //assertion
+    expect(add(2, 2)).toBe(4);
+});
